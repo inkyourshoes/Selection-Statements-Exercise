fd8a39649bcb54eaf42dcac3535d74cd8086a9af
inkyourshoesgit config user.namegit config --global user.email inkyourshoes@gmail.comglobal user.name inkyourshoes <inkyourshoes@gmail.com>
2025-07-14 20:04:48 -0400

    finished selection statements

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -3,8 +3,33 @@
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        { 
+            var r = new Random();
+            var favNumber = r.Next(1,100);
+            int response = 0;
+            
+            //var userInput = int.Parse(Console.ReadLine());
+
+            while (response != favNumber)
+            {
+
+                Console.WriteLine("Give me a number between 1 and 100:");
+                response = int.Parse(Console.ReadLine());
+
+                if (response < favNumber)
+                {
+                    Console.WriteLine("Too low!\nYour guess: {response}");
+                }
+                else if (response > favNumber)
+                {
+                    Console.WriteLine("Too high!\nYour guess: {response}");
+                }
+                else
+                {
+                    Console.WriteLine("You got it!\nYour guess: {response}");
+                }
+            }
+            
         }
     }
 }
